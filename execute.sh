0dfb075aa8b54664fc051da2279e09cb86ce83f4
sixfourscientific <sixfourscientific@outlook.com>
2026-07-13 11:04:28 +0100

    added execute script

---
+++ execute.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "~ RUNNING VIA SCRIPT ~"
+
+nextflow \
+    -C pipeline/nextflow.config \
+    run pipeline/stem.nf \
+    -latest \
+    -profile login \
+    -params-file pipeline/params/defaults.json \
+    --execute all \
+    --inputs pipeline/inputs/test/SampleInfoTest.tsv \
+    --supplementary ''
---
+++ work/69/d642bab7b4f62f067101e35c7a5779/.command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ue
+mkdir -p subDir
+echo 'TEST' > OUTPUT1.txt
+echo 'TEST' > subDir/OUTPUT2.txt
