ad6d0df1feca144c8cd0e527ab1a3b4f2a512e93
sixfourscientific <sixfourscientific@outlook.com>
2026-07-28 09:15:04 +0100

    removed test directory

--- test/workDir/00/3ffbbd07ad33952ef3dc97da5524ae/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-mkdir -p subDir
-echo 'DATA: sample2' > OUTPUT1.txt
-echo 'DATA: sample2' > subDir/OUTPUT2.txt
--- test/workDir/86/354f791f31d6add54f3c0e154ed6e8/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-mkdir -p subDir
-echo 'DATA: sample1' > OUTPUT1.txt
-echo 'DATA: sample1' > subDir/OUTPUT2.txt
