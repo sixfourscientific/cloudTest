52f649eba08021aa101d8ed74689a42d3d40f179
sixfourscientific <sixfourscientific@outlook.com>
2026-07-31 21:56:26 +0100

    switch launch to aws

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -3,7 +3,9 @@
 REPO_DIR=$(dirname $(realpath "$0"))
 
 $REPO_DIR/LaunchWorkflow.sh \
-	-s local \
+	-s aws_batch \
 	-p defaults \
 	-x data \
+	-o s3://test-547154048962-eu-north-1/nextflow-outputs \
+	-w s3://test-547154048962-eu-north-1/nextflow-work \
 	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestLocal.tsv $@
