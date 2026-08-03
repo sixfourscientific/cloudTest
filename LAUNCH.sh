9f96e7c5c3fb34049677c397539db11a516b7362
sixfourscientific <sixfourscientific@outlook.com>
2026-08-03 11:13:10 +0100

    renamed aws_batch profile to aws to avoid _ parse issue in Launch script

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -3,9 +3,8 @@
 REPO_DIR=$(dirname $(realpath "$0"))
 
 $REPO_DIR/LaunchWorkflow.sh \
-	-s aws_batch \
+	-s local \
 	-p defaults \
+	-a archive \
 	-x data \
-	-o s3://test-547154048962-eu-north-1/nextflow-outputs \
-	-w s3://test-547154048962-eu-north-1/nextflow-work \
 	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestLocal.tsv $@
