b33f3d3f590bba381ee9fdafb7ec072669d66366
sixfourscientific <sixfourscientific@outlook.com>
2026-08-04 19:54:07 +0100

    changed launch to awsbatch

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -3,7 +3,8 @@
 REPO_DIR=$(dirname $(realpath "$0"))
 
 $REPO_DIR/LaunchWorkflow.sh \
-	-s local \
+	-s awsbatch \
 	-p defaults \
 	-x data \
+    -b s3://core-547154048962-eu-west-2/nextflow-TEST \
 	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestLocal.tsv $@
