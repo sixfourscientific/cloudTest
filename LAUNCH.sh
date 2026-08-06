222a7c1da59cae26620d529e6226d789402df0f6
sixfourscientific <sixfourscientific@outlook.com>
2026-08-06 10:36:10 +0100

    removed aws testing parameters

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -6,6 +6,5 @@ $REPO_DIR/LaunchWorkflow.sh \
 	-s awsbatch \
 	-p defaults \
 	-x data \
-	-r launch_awsbatch_defaults_2026.08.05_14.29.48 \
-	-b s3://core-547154048962-eu-west-2/nextflow-TEST \
+	-b s3://core-547154048962-eu-west-2/nextflow \
 	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestLocal.tsv $@
