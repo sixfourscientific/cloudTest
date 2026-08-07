0bd4dcd8fc7a4ff8bc0d78207f3248a2793c9bad
sixfourscientific <sixfourscientific@outlook.com>
2026-08-07 16:30:26 +0100

    testing paths

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -5,6 +5,6 @@ REPO_DIR=$(dirname $(realpath "$0"))
 $REPO_DIR/LaunchWorkflow.sh \
 	-s local \
 	-p defaults \
-	-x data \
+	-x paths \
 	-b s3://core-547154048962-eu-west-2/nextflow \
-	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestLocal.tsv $@
+	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestS3.tsv $@
