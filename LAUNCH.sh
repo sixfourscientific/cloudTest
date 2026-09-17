3c08e7bcd054e489e9375906a1185dbfe270ec37
sixfourscientific <sixfourscientific@outlook.com>
2026-09-17 08:38:12 +0100

    ammended afterscript task log cp for aws & gcp

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -2,9 +2,12 @@
 
 REPO_DIR=$(dirname $(realpath "$0"))
 
+#BUCKET="-b s3://core-547154048962-eu-west-2/nextflow"
+BUCKET="-b gs://core-sixfourscientific-europe-west2/nextflow"
+
 $REPO_DIR/LaunchWorkflow.sh \
-	-s awsbatch \
+	-s googlebatch \
 	-p defaults \
 	-x data \
-    -b s3://core-547154048962-eu-west-2/nextflow \
+	$BUCKET \
 	-i $REPO_DIR/pipeline/inputs/test/SampleInfoTestLocal.tsv $@
